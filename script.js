d9f6dca7958d71b96f3de8b35ece8bae3df3dd73
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2023-11-03 13:17:15 +0100

    Update Timer Optimizacion

--- script.js
+++ script.js
@@ -1,74 +1,63 @@
 window.onload = () => {
-    btnstart = document.getElementById("start");
-    btnstart.addEventListener("click",startbottom);
+    let btnstart = document.getElementById("start");
+    btnstart.addEventListener("click", startbottom);
 
-    btnstop = document.getElementById("stop");
+    let btnstop = document.getElementById("stop");
     btnstop.addEventListener("click", stopbottom);
 
-    btnreset = document.getElementById("reset");
+    let btnreset = document.getElementById("reset");
     btnreset.addEventListener("click", resetbottom);
-}
-let run = false;
-seconds = 0;
-minutes = 0;
-hours = 0; 
-let intervalo;
+  };
 
-function startbottom(){
-    if (!run){
-        run = true;
-        btnstart.disabled = true;
-        intervalo = setInterval(secondsTimer, 1000);
-        intervalo2 = setInterval(minutesTimer, 60000);
+  let run = false;
+  let miliseconds = 0;
+  let seconds = 0;
+  let minutes = 0;
+  let intervalo;
+
+  function startbottom() {
+    if (!run) {
+      run = true;
+      document.getElementById("start").disabled = true;
+      intervalo = setInterval(Timer, 10);
     }
+  }
 
-}
-
-function stopbottom() {
+  function stopbottom() {
     run = false;
-    btnstart.disabled = false;
+    document.getElementById("start").disabled = false;
     clearInterval(intervalo);
-    clearInterval(intervalo2);
-}
+  }
 
-function resetbottom() {
-    seconds = document.getElementById("seconds").innerHTML = "00";
-    minutes = document.getElementById("minutes").innerHTML = "00";
+  function resetbottom() {
+    miliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    document.getElementById("miliseconds").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+  }
 
-}
-
-function secondsTimer() {
+  function Timer() {
     if (run) {
-        if(seconds == 59){
-            seconds = -1;
-        }
-        else{
-            seconds++;
-            if (seconds < 10) { 
-                seconds = "0"+seconds;
-            }
-            secondsdisplay = document.getElementById("seconds").innerHTML = seconds;
-            secondsdisplay.innerHTML = ":"+seconds;
-        }
-        
+      miliseconds++;
+      if (miliseconds == 100) {
+        miliseconds = 0;
+        seconds++;
+      }
+
+      if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+      }
+
+      let milisecondsDisplay = document.getElementById("miliseconds");
+      milisecondsDisplay.innerHTML = miliseconds < 10 ? "0" + miliseconds : miliseconds;
+      
+      let secondsDisplay = document.getElementById("seconds");
+      secondsDisplay.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+      
+      let minutesDisplay = document.getElementById("minutes");
+      minutesDisplay.innerHTML = minutes < 10 ? "0" + minutes : minutes;
     }
-
-
-}
-
-function minutesTimer() {
-    if (run) {
-        if(minutes == 59){
-            minutes = 0;
-        }
-        else{
-            minutes++;
-            if (minutes < 10) { 
-                minutes = "0"+minutes;
-            }
-            minutesdisplay = document.getElementById("minutes").innerHTML = minutes;
-            minutesdisplay.innerHTML = ":"+minutes;
-        }
-    }
-}
-
+  }
