b576da40e21a415d7e743d07aca189d49a8bb5ce
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2023-11-03 12:42:11 +0100

    Update

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function startbottom(){
         btnstart.disabled = true;
         intervalo = setInterval(secondsTimer, 1000);
         intervalo2 = setInterval(minutesTimer, 60000);
-        intervalo3 = setInterval(hoursTimer, 3600000);
     }
 
 }
@@ -30,47 +29,46 @@ function stopbottom() {
     btnstart.disabled = false;
     clearInterval(intervalo);
     clearInterval(intervalo2);
-    clearInterval(intervalo3);
 }
 
 function resetbottom() {
     seconds = document.getElementById("seconds").innerHTML = "00";
     minutes = document.getElementById("minutes").innerHTML = "00";
-    hours = document.getElementById("hours").innerHTML = "00";
 
 }
+
 function secondsTimer() {
     if (run) {
-        if(seconds == 58){
-            seconds = 1;
+        if(seconds == 59){
+            seconds = -1;
         }
         else{
             seconds++;
+            if (seconds < 10) { 
+                seconds = "0"+seconds;
+            }
             secondsdisplay = document.getElementById("seconds").innerHTML = seconds;
+            secondsdisplay.innerHTML = ":"+seconds;
         }
+        
     }
+
+
 }
 
 function minutesTimer() {
     if (run) {
         if(minutes == 59){
-            minutes = 1;
+            minutes = 0;
         }
         else{
             minutes++;
+            if (minutes < 10) { 
+                minutes = "0"+minutes;
+            }
             minutesdisplay = document.getElementById("minutes").innerHTML = minutes;
+            minutesdisplay.innerHTML = ":"+minutes;
         }
     }
 }
 
-function hoursTimer() {
-    if (run) {
-        if(hours == 10){
-            document.write("Suficiente");
-        }
-        else{
-            hours++;
-            hoursdisplay = document.getElementById("seconds").innerHTML = hours;
-        }
-    }
-}
